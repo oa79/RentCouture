e7bc568d464e2848f3835fff56fe2d490e8b9e1a
Omar Aslam <omar.aslam79@gmail.com>
2016-03-27 09:41:43 +0400

    Client Side Folder

---
+++ client/js/factories/customerFactory.js
@@ -0,0 +1,26 @@
+var myApp = angular.module('myApp', ['ngRoute', 'ngMessages']);
+
+myApp.factory('customerFactory', function($http){
+	var customers = [];
+	var factory = {};
+
+	factory.getCustomers = function(callback){
+		$http.get('/customers').success(function(output){
+			callback(output);
+		});
+	};
+	factory.recent_show = function(callback){
+		$http.get('/recent_customers').success(function(output){
+			callback(output);
+		});
+	};
+	factory.addCustomer = function(new_customer, callback){
+		$http.post('/addCustomer', new_customer).success(function(output){
+			
+			callback(output);
+		});
+	}
+
+
+	return factory;
+});
